b650c1c2fd4a2d183beec2d42b89934b9a441399
abhay <abhay.k@techjini.com>
2018-04-17 12:03:10 +0530

    IP

--- JavaLearning/MainClass.java
+++ JavaLearning/MainClass.java
@@ -1,25 +1,12 @@
 package JavaLearning;
 
-import java.util.Scanner;
+import java.net.InetAddress;
 
 public class MainClass {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        float pi = (float) 3.142, res;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the first input value");
-        int val1 = scanner.nextInt();
-
-        Scanner scanner2 = new Scanner(System.in);
-        System.out.println("Enter the second input value");
-        int val2 = scanner2.nextInt();
-
-        res = (val1 * val2)/pi;
-
-        System.out.println("---- ---- ---- ----");
-        System.out.println("The result of the calculation is " + res);
-        System.out.println("---- ---- ---- ----");
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        System.out.println("The IP address is " + inetAddress.getHostAddress());
     }
 }
